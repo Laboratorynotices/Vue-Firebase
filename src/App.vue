d61c290a275c863c493846bb0eb829d2094aa43c
Laboratorynotices <muenchen.webas@gmail.com>
2023-08-08 00:56:07 +0200

    feat: Подсоединение к Firebase
    Подсоединение к Firebase, а так же в качестве теста в консоле отображем список данных от туда.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/firebase/config";
+
+onMounted(async () => {
+  const querySnapshot = await getDocs(collection(db, "groups"));
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data());
+  });
+});
+</script>
+
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
