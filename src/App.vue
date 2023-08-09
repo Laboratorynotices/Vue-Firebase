9590670cf5fe4e942cebecd217647836d22e272a
Laboratorynotices <muenchen.webas@gmail.com>
2023-08-09 14:59:53 +0200

    feat: Добавил каркас
    Добавил один из каркасов Vuetify Constrained

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { onMounted } from "vue";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "@/firebase/config";
+import LayoutMain from "@/layouts/constrained/LayoutMain.vue";
 
 onMounted(async () => {
   const querySnapshot = await getDocs(collection(db, "groups"));
@@ -13,32 +14,5 @@ onMounted(async () => {
 </script>
 
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <layout-main />
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
