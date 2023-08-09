049833b90fc1e2bce5a650e74ecd90b071b85541
Laboratorynotices <muenchen.webas@gmail.com>
2023-08-09 18:55:37 +0200

    feat: Убрал левый столбик
    Перенёс код левого столбика в отдельный компонент, но сделал так, чтобы он не выводился

--- src/layouts/constrained/LayoutMain.vue
+++ src/layouts/constrained/LayoutMain.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import AppBarNav from "@/layouts/constrained/AppBarNav.vue";
+import AppMainLeftCol from "@/layouts/constrained/AppMainLeftCol.vue";
 </script>
 
 <template>
@@ -26,21 +27,7 @@ import AppBarNav from "@/layouts/constrained/AppBarNav.vue";
     <v-main class="bg-grey-lighten-3">
       <v-container>
         <v-row>
-          <v-col cols="2">
-            <v-sheet rounded="lg">
-              <v-list rounded="lg">
-                <v-list-item v-for="n in 5" :key="n" link>
-                  <v-list-item-title>List Item {{ n }}</v-list-item-title>
-                </v-list-item>
-
-                <v-divider class="my-2"></v-divider>
-
-                <v-list-item link color="grey-lighten-4">
-                  <v-list-item-title>Refresh</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-sheet>
-          </v-col>
+          <app-main-left-col></app-main-left-col>
 
           <v-col>
             <v-sheet min-height="70vh" rounded="lg">
