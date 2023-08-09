fd37600a9e6571095be47ea6276ce7adfe299b49
Laboratorynotices <muenchen.webas@gmail.com>
2023-08-09 18:36:02 +0200

    style: Новый компонент для навигации
    Вынес код с навигацией в отдельный компонент, чтобы не весь код был в одном файле.

---
+++ src/layouts/constrained/AppBarNav.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+/**
+ * Структура данных отдельного пунка (основной) навигации.
+ * Позже будет перенесён в отдельный файл для типов (структур).
+ */
+interface RouterLink {
+  // Отображаемое название в навигации
+  name: string;
+  // Относительный путь
+  to: string;
+}
+
+/**
+ * Данные для основной навигации.
+ * Позже будут перенесены в отдельный файл,
+ * чтобы от туда маршрутизатор брал информацию для настроек.
+ */
+const navElements: Array<RouterLink> = [
+  {
+    name: "Home",
+    to: "/",
+  },
+  {
+    name: "About",
+    to: "/about",
+  },
+];
+</script>
+
+<template>
+  <nav>
+    <v-btn
+      v-for="(navElement, i) in navElements"
+      :key="i"
+      variant="text"
+      :to="navElement.to"
+    >
+      {{ navElement.name }}
+    </v-btn>
+  </nav>
+</template>
--- src/layouts/constrained/LayoutMain.vue
+++ src/layouts/constrained/LayoutMain.vue
@@ -1,30 +1,5 @@
 <script setup lang="ts">
-/**
- * Структура данных отдельного пунка (основной) навигации.
- * Позже будет перенесён в отдельный файл для типов (структур).
- */
-interface RouterLink {
-  // Отображаемое название в навигации
-  name: string;
-  // Относительный путь
-  to: string;
-}
-
-/**
- * Данные для основной навигации.
- * Позже будут перенесены в отдельный файл,
- * чтобы от туда маршрутизатор брал информацию для настроек.
- */
-const navElements: Array<RouterLink> = [
-  {
-    name: "Home",
-    to: "/",
-  },
-  {
-    name: "About",
-    to: "/about",
-  },
-];
+import AppBarNav from "@/layouts/constrained/AppBarNav.vue";
 </script>
 
 <template>
@@ -34,16 +9,7 @@ const navElements: Array<RouterLink> = [
         <v-avatar class="me-10 ms-4" color="grey-darken-1" size="32"></v-avatar>
 
         <!-- Навигация. -->
-        <nav>
-          <v-btn
-            v-for="(navElement, i) in navElements"
-            :key="i"
-            variant="text"
-            :to="navElement.to"
-          >
-            {{ navElement.name }}
-          </v-btn>
-        </nav>
+        <app-bar-nav></app-bar-nav>
 
         <v-spacer></v-spacer>
 
