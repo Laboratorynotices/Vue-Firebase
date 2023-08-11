b570cd76766f8bacda8d6d6a58e243953c5d61ab
Laboratorynotices <muenchen.webas@gmail.com>
2023-08-11 18:05:50 +0200

    refactor: Перенос запроса кода в GroupsIndexView

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,5 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "@/firebase/config";
 import LayoutMain from "@/layouts/constrained/LayoutMain.vue";
-
-onMounted(async () => {
-  const querySnapshot = await getDocs(collection(db, "groups"));
-  querySnapshot.forEach((doc) => {
-    // doc.data() is never undefined for query doc snapshots
-    console.log(doc.id, " => ", doc.data());
-  });
-});
 </script>
 
 <template>
