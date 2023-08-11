785cc807f77b09ea91db177e1ba2a0322b510605
Laboratorynotices <muenchen.webas@gmail.com>
2023-08-11 19:56:14 +0200

    style: Удовлетворяем Prettier

--- src/firebase/groups.ts
+++ src/firebase/groups.ts
@@ -4,7 +4,7 @@ import { collection, getDocs } from "firebase/firestore";
 /**
  * Возвращает все документы "групп", что записаны в Firebase.
  */
-export const getGroups = async () : Promise<void> => {
+export const getGroups = async (): Promise<void> => {
   const querySnapshot = await getDocs(collection(db, "groups"));
   querySnapshot.forEach((doc) => {
     // doc.data() is never undefined for query doc snapshots
