1cf58d83cbb19aa3620de6e7b8797ef95b3ef1fa
Laboratorynotices <muenchen.webas@gmail.com>
2023-08-11 19:53:04 +0200

    style: Опечатка и формализация TS

--- src/firebase/groups.ts
+++ src/firebase/groups.ts
@@ -2,9 +2,9 @@ import { db } from "./config";
 import { collection, getDocs } from "firebase/firestore";
 
 /**
- * Возвращает все документы "крупп", что записаны в Firebase.
+ * Возвращает все документы "групп", что записаны в Firebase.
  */
-export const getGroups = async () => {
+export const getGroups = async () : Promise<void> => {
   const querySnapshot = await getDocs(collection(db, "groups"));
   querySnapshot.forEach((doc) => {
     // doc.data() is never undefined for query doc snapshots
