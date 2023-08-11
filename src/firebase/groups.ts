d9f0b616447b4ca17764f14ab4033f7765fc0863
Laboratorynotices <muenchen.webas@gmail.com>
2023-08-11 20:22:30 +0200

    feat: Отображаются документы из коллекции groups
    Теперь содержимое коллекции groups отображается на странице, а не в логах терминала.

--- src/firebase/groups.ts
+++ src/firebase/groups.ts
@@ -1,13 +1,17 @@
+import { Group } from "@/types";
 import { db } from "./config";
 import { collection, getDocs } from "firebase/firestore";
 
 /**
  * Возвращает все документы "групп", что записаны в Firebase.
  */
-export const getGroups = async (): Promise<void> => {
+export const getGroups = async (): Promise<Group[]> => {
   const querySnapshot = await getDocs(collection(db, "groups"));
+  // Объявляем переменную, куда будем собирать прочитанные данные.
+  const groups: Group[] = [];
   querySnapshot.forEach((doc) => {
-    // doc.data() is never undefined for query doc snapshots
-    console.log(doc.id, " => ", doc.data());
+    // По очереди добавляем данные в переменную
+    groups.push({ id: doc.id, ...doc.data() } as Group);
   });
+  return groups;
 };
--- src/views/GroupsIndexView.vue
+++ src/views/GroupsIndexView.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { Ref, onMounted, ref } from "vue";
 import { getGroups } from "@/firebase/groups";
+import { Group } from "@/types";
+
+// Указатель на хранилище групп
+const groups: Ref = ref<Group[]>([]);
 
 onMounted(async () => {
-  getGroups();
+  groups.value = await getGroups();
 });
 </script>
 
 <template>
-  <v-card width="400" class="mx-auto">
+  <v-card
+    v-for="group in groups"
+    :key="group.id"
+    width="400"
+    class="mx-auto my-6"
+  >
     <v-card-item>
-      <v-card-title>Заголовок</v-card-title>
+      <v-card-title>{{ group.name }}</v-card-title>
     </v-card-item>
 
     <v-card-text>Текст</v-card-text>
